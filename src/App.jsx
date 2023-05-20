1bb8e0cdca21d770135b001e10261041470dee24
SoumyadeepOSD <opencontainer255@gmail.com>
2023-05-20 20:28:05 +0530

    user tech-stack API integration [done]

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-// import {useState} from 'react';
+import {useState} from 'react';
 
 
+import REACT_APP_API_KEY from "../env";
 const App = () => {
-  // const[data, setData] = useState('');
-  const URL = "https://cache.showwcase.com/urlmeta?url=https://www.showwcase.com/thread/98763"
+  const init = [];
+  const[username, setUsername] = useState(init);
+  const[userdata, setUserdata]=useState([]);
+  const URL = `https://cache.showwcase.com/user/${username}/stacks`;
 const FetchData = async() => {
   fetch(URL,
     {
       headers: {
-        'x-api-key': '858ee679f0c978c9497d849710127ec2ac50b9656842d994f5',
+        'x-api-key': REACT_APP_API_KEY,
       },
     })
   .then((response) => {
@@ -19,16 +22,34 @@ const FetchData = async() => {
     }
   })
   .then(data => {
-    console.log(data);
-    // setData(data.available);
+  //  console.log(data[0].stack.description);
+  //  setData(data)
+  // console.log(data);
+    setUserdata(data);
+    console.log(userdata[0]);
   })
   .catch((error) => console.error("FETCH ERROR:", error));
 }
 
   return (
     <div>
+      <form>
+        <input 
+        placeholder='Enter your username'
+        onChange={e => setUsername(e.target.value)}/>
+      </form>
+      <h1>{username}</h1>
       <button onClick={FetchData}>Press</button>
-      {/* <div style={{color:'white'}}>{data.available}</div> */}
+      {
+       userdata.map((value, index)=>{
+        return(
+          <div key={index} style={{display:'flex', flexDirection:'row', alignItems:'center'}}>
+            <h2>{value.stack.name}</h2>
+            <img src={value.stack.iconUrl} style={{height:20, width:20}} alt={value.stack.name}/>
+          </div>
+        );
+       })
+      }
     </div>
   )
 }
